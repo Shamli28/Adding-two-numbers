eff88e4cff5373a74caa1eb6a211c386204be570
Shamli Shankarappa Balashetty <shamlibalashetty27@gmail.com>
2021-06-22 18:10:26 +0530

    Add files via upload

---
+++ Addition of Two Numbers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+# In[2]:
+
+
+# Adding Two Numbers With User Input
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+# In[ ]:
+
+
+
+
